de4a4280b73b24e1b714986be4ed08f0059bdd28
paulo <pauloeduardob75@gmail.com>
2026-08-14 17:38:38 -0300

    first commit

--- Patta/ContentView.swift
+++ Patta/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "pawprint.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, pata!")
+            Text("Hello, Pata!")
         }
         .padding()
     }
