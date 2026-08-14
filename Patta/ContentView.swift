496758a9dc49d91c87462a4bc46ab227a0e41454
paulo <pauloeduardob75@gmail.com>
2026-08-14 17:36:54 -0300

    Initial Commit

---
+++ Patta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Patta
+//
+//  Created by Paulo Eduardo Barbosa da Silva on 14/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "pawprint.fill")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, pata!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Patta/PattaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PattaApp.swift
+//  Patta
+//
+//  Created by Paulo Eduardo Barbosa da Silva on 14/08/26.
+//
+
+import SwiftUI
+
+@main
+struct PattaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
