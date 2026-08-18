c44a4435d3f522d0992dbc587daccd10ecfb537d
AlexeyFrens <leiloka46@gmail.com>
2026-08-18 18:34:12 -0300

    feat: Implementacao de Adicionar Pet e Tela de Pets

---
+++ Patta/Feature/Pets/Components/PetCard.swift
@@ -0,0 +1,18 @@
+//
+//  PetCard.swift
+//  Patta
+//
+//  Created by João Cláudio dos Santos Souza on 18/08/26.
+//
+
+import SwiftUI
+
+struct PetCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PetCard()
+}
