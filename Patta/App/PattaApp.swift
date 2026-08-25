f54f18b9c233fae1b8e0ee03bed7e59eea09c552
AlexeyFrens <leiloka46@gmail.com>
2026-08-25 17:55:14 -0300

    feat: Adicionando Vacinas Padrao no App

--- Patta/App/PattaApp.swift
+++ Patta/App/PattaApp.swift
@@ -55,6 +55,8 @@ struct PattaApp: App {
             _vaccineRegistryListStore = .init(initialValue: vaccineRegistryStore)
             _vaccineRegistryViewModel = .init(initialValue: VaccineRegistryViewModel(store: vaccineRegistryStore))
             _vaccineViewModel = .init(initialValue: VaccineViewModel(store: vaccineStore))
+            
+            seedDefaultVaccines()
         } catch {
             fatalError("Erro ao inicializar o ModelContainer: \(error.localizedDescription)")
         }
@@ -73,5 +75,115 @@ struct PattaApp: App {
                 .environment(vaccineRegistryListStore)
         }
         .modelContainer(container)
+    }
+    
+    @MainActor
+    private func seedDefaultVaccines() {
+        let context = container.mainContext
+        
+        do {
+            let descriptor = FetchDescriptor<VaccineModel>()
+            let existingCount = try context.fetchCount(descriptor)
+            
+            if existingCount == 0 {
+                let defaultVaccines = [
+                    "V8 (Óctupla Canina)",
+                    "V10 (Décupla Canina)",
+                    "V12 (Duodécupla Canina)",
+                    "Antirrábica Canina",
+                    "Giárdia Canina",
+                    "Gripe Canina",
+                    "Leishmaniose Canina",
+                    "Coronavirose Canina",
+                    "Parvovirose Canina",
+                    "Cinomose",
+                    "Hepatite Infecciosa Canina (Adenovírus tipo 1)",
+                    "Leptospirose Canina",
+                    "Parainfluenza Canina",
+                    "Traqueobronquite Infecciosa Canina",
+                    "V3 (Tríplice Felina)",
+                    "V4 Felina",
+                    "V5 Felina",
+                    "Leucemia Felina (FeLV)",
+                    "Antirrábica Felina",
+                    "Clamidiose Felina",
+                    "Peritonite Infecciosa Felina (PIF/FIP)",
+                    "Panleucopenia Felina",
+                    "Rinotraqueíte Felina (Herpesvírus Felino)",
+                    "Calicivirose Felina",
+                    "Imunodeficiência Felina (FIV)",
+                    "Doença de Newcastle",
+                    "Bouba Aviária (Varíola Aviária)",
+                    "Influenza Aviária",
+                    "Bronquite Infecciosa Aviária",
+                    "Doença de Marek",
+                    "Gumboro (Bursite Infecciosa)",
+                    "Reovirose Aviária",
+                    "Encefalomielite Aviária",
+                    "Clamidiose Aviária (Psitacose)",
+                    "Poliomavírus Aviário (PBFD associado)",
+                    "Circovírus Aviário (PBFD)",
+                    "Coriza Infecciosa das Aves",
+                    "Laringotraqueíte Infecciosa Aviária",
+                    "Mixomatose",
+                    "Doença Hemorrágica Viral do Coelho (VHD/RHDV)",
+                    "Pasteurelose do Coelho",
+                    "Cinomose (Furão)",
+                    "Antirrábica (Furão)",
+                    "Enterite Epizoótica do Furão",
+                    "Influenza Equina",
+                    "Tétano Equino",
+                    "Encefalomielite Equina do Leste",
+                    "Encefalomielite Equina do Oeste",
+                    "Raiva Equina",
+                    "Rinopneumonite Equina",
+                    "Arterite Viral Equina",
+                    "Febre do Nilo Ocidental",
+                    "Adenite Equina (Garrotilho)",
+                    "Rotavirose Equina",
+                    "Febre Aftosa",
+                    "Brucelose Bovina",
+                    "Raiva dos Herbívoros (Bovina)",
+                    "Clostridiose Bovina (Polivalente)",
+                    "Botulismo Bovino",
+                    "IBR (Rinotraqueíte Infecciosa Bovina)",
+                    "BVD (Diarreia Viral Bovina)",
+                    "Leptospirose Bovina",
+                    "Carbúnculo Sintomático (Manqueira)",
+                    "Carbúnculo Hemático (Antraz)",
+                    "Pasteurelose Bovina",
+                    "Febre Catarral Maligna",
+                    "Peste Suína Clássica",
+                    "Parvovirose Suína",
+                    "Erisipela Suína",
+                    "Circovirose Suína (PCV2)",
+                    "Micoplasmose Suína",
+                    "Rinite Atrófica Suína",
+                    "Colibacilose Suína",
+                    "Síndrome Reprodutiva e Respiratória Suína (PRRS)",
+                    "Clostridiose Ovina/Caprina (Polivalente)",
+                    "Ectima Contagioso",
+                    "Linfadenite Caseosa",
+                    "Raiva dos Herbívoros (Ovina/Caprina)",
+                    "Brucelose Caprina",
+                    "Vibriose (Peixes)",
+                    "Furunculose (Salmonídeos)",
+                    "Septicemia Hemorrágica Viral (Peixes)",
+                    "Mixomatose (Roedores)",
+                    "Antirrábica (Cavalos-marinhos)",
+                    "Doença Respiratória Crônica (Roedores)",
+                    "Pasteurelose (Porquinho-da-índia)",
+                ]
+                
+                for title in defaultVaccines {
+                    let newVaccine = VaccineModel(id: UUID(), title: title)
+                    context.insert(newVaccine)
+                }
+                
+                try context.save()
+            }
+        } catch {
+            print("Erro ao popular vacinas padrão: \(error.localizedDescription)")
+        }
     }
 }
